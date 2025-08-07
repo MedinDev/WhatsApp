0a7e4ab85d6e46ee935f047c195aa6bae8666630
medtexprog <medtexprog@gmail.com>
2025-08-07 19:14:37 +0800

    version02

--- App.js
+++ App.js
@@ -1,13 +1,66 @@
-import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text} from 'react-native';
+import {StyleSheet, Text} from "react-native";
 import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
+import * as SplashScreen from 'expo-splash-screen';
+import {useCallback, useEffect, useState} from "react";
+import * as Font from 'expo-font';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+
+    const [appIsLoaded, setAppIsLoaded] = useState(false);
+
+    useEffect(() => {
+        // Load fonts
+        setTimeout(() => {
+            setAppIsLoaded(true);
+        }, 2000);
+    });
+
+    useEffect(() => {
+        const prepare = async () => {
+            try {
+                await Font.loadAsync({
+                    "black": require("./assets/fonts/Roboto-Black.ttf"),
+                    "blackItalic": require("./assets/fonts/Roboto-BlackItalic.ttf"),
+                    "bold": require("./assets/fonts/Roboto-Bold.ttf"),
+                    "boldItalic": require("./assets/fonts/Roboto-BoldItalic.ttf"),
+                    "italic": require("./assets/fonts/Roboto-Italic.ttf"),
+                    "light": require("./assets/fonts/Roboto-Light.ttf"),
+                    "lightItalic": require("./assets/fonts/Roboto-LightItalic.ttf"),
+                    "medium": require("./assets/fonts/Roboto-Medium.ttf"),
+                    "mediumItalic": require("./assets/fonts/Roboto-MediumItalic.ttf"),
+                    "regular": require("./assets/fonts/Roboto-Regular.ttf"),
+                    "thin": require("./assets/fonts/Roboto-Thin.ttf"),
+                    "thinItalic": require("./assets/fonts/Roboto-ThinItalic.ttf"),
+                });
+            } catch (error) {
+                console.log.error();
+            } finally {
+                setAppIsLoaded(true)
+            }
+        }
+        prepare();
+    }, []);
+
+    const onLayout = useCallback(async () => {
+        if (appIsLoaded) {
+            await SplashScreen.hideAsync();
+        }
+    }, [appIsLoaded]);
+
+    if (!appIsLoaded) {
+        return null;
+    }
+
     return (
-        <SafeAreaProvider style={styles.container}>
+        <SafeAreaProvider
+            style={styles.container}
+            onLayout={onLayout}>
             <SafeAreaView>
-                <Text>Open up App.js </Text>
-                <StatusBar style="auto"/>
+
+                <Text style={styles.label}>Hi everyone!</Text>
+
             </SafeAreaView>
         </SafeAreaProvider>
     );
@@ -16,8 +69,14 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
+        backgroundColor: "#fff",
         justifyContent: 'center',
+        alignItems: 'center',
+
     },
+    label: {
+        color: 'black',
+        fontFamily: 'bold',
+        fontSize: 18
+    }
 });
